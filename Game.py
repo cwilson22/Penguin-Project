bebfec435e4d622e133898857f27e3e642d9b8a4
cwilson22 <csw8@buffalo.edu>
2020-03-14 17:31:10 -0400

    Levels & numbers on overlays

--- Game.py
+++ Game.py
@@ -5,9 +5,11 @@ import game_classes
 import constants
 import pygameMenu
 import pygame.font
+import json
 
 
 pygame.init()
+pygame.mouse.set_visible(False)
 
 # Initialize constants
 CLOCK = pygame.time.Clock()
@@ -17,17 +19,30 @@ GRASS = pygame.image.load('sprites/tiles/tile_dirt.png')
 TILE_SIZE = constants.game['tile_size']
 ANIMATION_MAX_CT = constants.game['animate_ct']
 FPS = constants.game['game_fps']
+OVERLAY_NUMBERS_X = [constants.game['numberX_locations_top'], constants.game['numberX_locations_bottom']]
+OVERLAY_NUMBERS_Y = [constants.game['numberY_location_top'], constants.game['numberY_location_bottom']]
+OVERLAY_RESOURCES = [constants.game['overlay1_resources'], constants.game['overlay2_resources']]
+OVERLAYS = []
+for k in range(12):
+    OVERLAYS.append(pygame.image.load('sprites/overlays/overlay_' + str(k) + '.png'))
 TILES = []
 for k in range(1, 6):
     TILES.append(pygame.image.load('sprites/tiles/tile_' + str(k) + '.png'))
 CONSTRUCTION_MENU = []
 for k in range(4):
     CONSTRUCTION_MENU.append(pygame.image.load('text/construct/sprite_' + str(k) + '.png'))
+NUMS = []
+for k in range(10):
+    NUMS.append(pygame.image.load('text/digits/num_' + str(k) + '.png'))
 
-# Initialize player and starting buildings
+# Initialize player and buildings
 PLAYER = game_classes.Penguin()
 GAME_OBJECTS = [game_classes.Factory('cereal_factory')]
 RESOURCES = game_classes.ResourceManager()
+with open('savestate.txt', 'r') as f:
+    save = json.load(f)
+for k in range(len(constants.resources)):
+    RESOURCES.resources.append(save[str(k)])
 GAME_STATE = 0
 
 # Set up screen surfaces
@@ -39,12 +54,42 @@ SMALLEST_SIDE = min(NUM_TILES[1] * SCREEN_WIDTH / NUM_TILES[0], SCREEN_HEIGHT)
 SCREEN_SURFACE = pygame.Surface((NUM_TILES[0] * SMALLEST_SIDE // NUM_TILES[1], SMALLEST_SIDE))
 
 
+def blit_levels():
+    for ob in GAME_OBJECTS:
+        ct = 3
+        x = ob.level
+        while x > 0:
+            if ct == 3:
+                GAME_SURFACE.blit(NUMS[x % 10], (ob.x + ob.num3_offset, ob.y + ob.numY_offset))
+            elif ct == 2:
+                GAME_SURFACE.blit(NUMS[x % 10], (ob.x + ob.num2_offset, ob.y + ob.numY_offset))
+            elif ct == 1:
+                GAME_SURFACE.blit(NUMS[x % 10], (ob.x + ob.num1_offset, ob.y + ob.numY_offset))
+            x = x // 10
+            ct -= 1
+
+
+def blit_overlay():
+    for t in range(len(OVERLAY_RESOURCES)):
+        for s in range(len(OVERLAY_RESOURCES[t])):
+            GAME_SURFACE.blit(OVERLAYS[s + 6*t], (TILE_SIZE * s, t * 121))
+            ct = 0
+            x = RESOURCES.resources[OVERLAY_RESOURCES[t][s]]
+            if x == 0:
+                GAME_SURFACE.blit(NUMS[0], (OVERLAY_NUMBERS_X[t][s], OVERLAY_NUMBERS_Y[t]))
+            else:
+                while x > 0:
+                    GAME_SURFACE.blit(NUMS[x % 10], (OVERLAY_NUMBERS_X[t][s] - (4*ct), OVERLAY_NUMBERS_Y[t]))
+                    x = x // 10
+                    ct += 1
+
+
 def blit_screen(game_size, animation_timer):
     for x in range(0, game_size[0], TILE_SIZE):
         for y in range(0, game_size[1], TILE_SIZE):
             GAME_SURFACE.blit(GRASS, (x, y))
-    for i in range(5):
-        GAME_SURFACE.blit(TILES[i], (32 * i, 0))
+    # for i in range(5):
+        # GAME_SURFACE.blit(TILES[i], (32 * i, 0))
 
     for game_object in GAME_OBJECTS:
         game_object.animate(animation_timer)
@@ -53,17 +98,21 @@ def blit_screen(game_size, animation_timer):
         else:
             GAME_SURFACE.blit(game_object.off_sprites[game_object.sprite_frame], (game_object.x, game_object.y))
 
-    if PLAYER.y_dir == 0 and PLAYER.x_dir == 0:
+    blit_levels()
+
+    if not PLAYER.key_up and not PLAYER.key_down and not PLAYER.key_left and not PLAYER.key_right:
         if PLAYER.facing_up:
             GAME_SURFACE.blit(PLAYER.still_back_sprites[PLAYER.sprite_frame], (PLAYER.x, PLAYER.y))
         else:
             GAME_SURFACE.blit(PLAYER.still_front_sprites[PLAYER.sprite_frame], (PLAYER.x, PLAYER.y))
-    elif PLAYER.y_dir < 0:
+    elif PLAYER.y_spd < -0.65:
         GAME_SURFACE.blit(PLAYER.back_sprites[PLAYER.sprite_frame], (PLAYER.x, PLAYER.y))
-    elif PLAYER.x_dir < 0:
+    elif PLAYER.x_spd < 0:
         GAME_SURFACE.blit(PLAYER.left_sprites[PLAYER.sprite_frame], (PLAYER.x, PLAYER.y))
     else:
         GAME_SURFACE.blit(PLAYER.front_sprites[PLAYER.sprite_frame], (PLAYER.x, PLAYER.y))
+
+    blit_overlay()
 
     pygame.transform.scale(GAME_SURFACE, (NUM_TILES[0] * SMALLEST_SIDE // NUM_TILES[1], SMALLEST_SIDE), SCREEN_SURFACE)
 
@@ -79,20 +128,14 @@ def blit_construction_menu():
 
     for fact in constants.factories.values():
         if PLAYER.calculate_tile() == fact['grid_pos']:
-            build1 = fact['cost_type1']
-            build2 = fact['cost_type2']
-            cost1 = fact['build_cost1']
-            cost2 = fact['build_cost2']
-            for i in constants.construct_menu['resources']:
-                if build1 == i:
-                    GAME_SURFACE.blit(pygame.image.load('text/construct/numbers/' + str(cost1) + '.png'),
-                                      (constants.construct_menu['offset_x'], constants.construct_menu['offset'+str(i)]))
-                elif build2 == i:
-                    GAME_SURFACE.blit(pygame.image.load('text/construct/numbers/' + str(cost2) + '.png'),
-                                      (constants.construct_menu['offset_x'], constants.construct_menu['offset'+str(i)]))
-                else:
-                    GAME_SURFACE.blit(pygame.image.load('text/construct/numbers/0.png'),
-                                      (constants.construct_menu['offset_x'], constants.construct_menu['offset'+str(i)]))
+            GAME_SURFACE.blit(pygame.image.load('text/construct/numbers/' + str(fact['build_cost1']) + '.png'),
+                              (constants.construct_menu['offset_x'], constants.construct_menu['offset1']))
+            GAME_SURFACE.blit(pygame.image.load('text/construct/numbers/' + str(fact['build_cost2']) + '.png'),
+                              (constants.construct_menu['offset_x'], constants.construct_menu['offset2']))
+            GAME_SURFACE.blit(pygame.image.load('text/construct/numbers/' + str(fact['build_cost3']) + '.png'),
+                              (constants.construct_menu['offset_x'], constants.construct_menu['offset3']))
+            GAME_SURFACE.blit(pygame.image.load('text/construct/numbers/' + str(fact['build_cost4']) + '.png'),
+                              (constants.construct_menu['offset_x'], constants.construct_menu['offset4']))
             break
 
     pygame.transform.scale(GAME_SURFACE, (NUM_TILES[0] * SMALLEST_SIDE // NUM_TILES[1], SMALLEST_SIDE), SCREEN_SURFACE)
@@ -129,44 +172,45 @@ def main():
                     if event.key == pygame.K_w:
                         PLAYER.facing_up = True
                         PLAYER.facing_left = False
-                        PLAYER.y_dir += 1
+                        PLAYER.key_up = False
                     if event.key == pygame.K_s:
                         PLAYER.facing_up = False
                         PLAYER.facing_left = False
-                        PLAYER.y_dir -= 1
+                        PLAYER.key_down = False
                     if event.key == pygame.K_a:
                         PLAYER.facing_left = True
                         PLAYER.facing_up = False
-                        PLAYER.x_dir += 1
+                        PLAYER.key_left = False
                     if event.key == pygame.K_d:
                         PLAYER.facing_left = False
                         PLAYER.facing_up = False
-                        PLAYER.x_dir -= 1
+                        PLAYER.key_right = False
                     if event.key == pygame.K_SPACE:
                         for ob in GAME_OBJECTS:
                             if PLAYER.calculate_tile() == ob.grid_pos:
                                 ob.flip_switch()
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_w:
-                        PLAYER.y_dir -= 1
+                        PLAYER.key_up = True
                     elif event.key == pygame.K_s:
-                        PLAYER.y_dir += 1
+                        PLAYER.key_down = True
                     elif event.key == pygame.K_a:
-                        PLAYER.x_dir -= 1
+                        PLAYER.key_left = True
                     elif event.key == pygame.K_d:
-                        PLAYER.x_dir += 1
+                        PLAYER.key_right = True
                     if event.key == pygame.K_q:
                         simple_menu.enable()
-                        PLAYER.x_dir = 0
-                        PLAYER.y_dir = 0
+                        PLAYER.stop()
                     if event.key == pygame.K_e:
-                        GAME_STATE = 1
-                        PLAYER.x_dir = 0
-                        PLAYER.y_dir = 0
+                        if PLAYER.calculate_tile() in RESOURCES.can_build:
+                            GAME_STATE = 1
+                            PLAYER.stop()
 
             PLAYER.move()
             blit_screen(GRID_SIZE, animation_frame)
             simple_menu.mainloop()
+            for ob in GAME_OBJECTS:
+                ob.produce()
             animation_frame += 1
             if animation_frame > ANIMATION_MAX_CT:
                 animation_frame = 0
@@ -181,13 +225,22 @@ def main():
                     if event.key == pygame.K_SPACE:
                         for fact in constants.factories.values():
                             if PLAYER.calculate_tile() == fact['grid_pos']:
-                                build1 = fact['cost_type1']
-                                build2 = fact['cost_type2']
+                                build1 = constants.construct_menu['resources'][0]
+                                build2 = constants.construct_menu['resources'][1]
+                                build3 = constants.construct_menu['resources'][2]
+                                build4 = constants.construct_menu['resources'][3]
                                 cost1 = fact['build_cost1']
                                 cost2 = fact['build_cost2']
-                                if RESOURCES.resources[build1] >= cost1 and RESOURCES.resources[build2] >= cost2:
+                                cost3 = fact['build_cost3']
+                                cost4 = fact['build_cost4']
+                                if RESOURCES.resources[build1] >= cost1 and RESOURCES.resources[build2] >= cost2 and \
+                                        RESOURCES.resources[build3] >= cost3 and RESOURCES.resources[build4] >= cost4:
                                     RESOURCES.resources[build1] -= cost1
                                     RESOURCES.resources[build2] -= cost2
+                                    RESOURCES.resources[build3] -= cost3
+                                    RESOURCES.resources[build4] -= cost4
+                                    GAME_OBJECTS.append(game_classes.Factory(fact['name']))
+                                    RESOURCES.can_build.remove(fact['grid_pos'])
                                 break
                         GAME_STATE = 0
         CLOCK.tick(FPS)
--- game_classes.py
+++ game_classes.py
@@ -1,6 +1,5 @@
 import constants
 import pygame
-import json
 
 
 class Penguin:
@@ -21,26 +20,48 @@ class Penguin:
         self.y = constants.penguin['start_y']
         self.x_offset = constants.penguin['center_px_x_offset']
         self.y_offset = constants.penguin['center_px_y_offset']
-        self.x_dir = 0
-        self.y_dir = 0
+        self.x_spd = 0
+        self.y_spd = 0
+        self.key_up = False
+        self.key_down = False
+        self.key_left = False
+        self.key_right = False
         self.facing_up = False
         self.facing_left = False
         self.sprite_frame = 0
         self.animation_count = 0
 
     def move(self):
-        if (self.x_dir < 0 and self.x + 5 > 0) or (self.x_dir > 0 and self.x + 15 < constants.game['grid_size'][0]):
-            self.x += self.x_dir
-        if (self.y_dir < 0 and self.y > 5) or (self.y_dir > 0 and self.y + 15 < constants.game['grid_size'][1] - 5):
-            self.y += self.y_dir
+        if self.key_left:
+            self.x_spd -= 0.4
+        if self.key_right:
+            self.x_spd += 0.4
+        if self.key_up:
+            self.y_spd -= 0.32
+        if self.key_down:
+            self.y_spd += 0.32
+        if ((self.x_spd < 0 and self. x + 5 > 0) and self.key_left) or ((self.x_spd > 0 and self.x + 15 < constants.game['grid_size'][0]) and self.key_right):
+            self.x += self.x_spd
+        if (self.y_spd < 0 and self.y > 5 and self.key_up) or (self.y_spd > 0 and self.y + 15 < constants.game['grid_size'][1] - 5 and self.key_down):
+            self.y += self.y_spd
+        self.x_spd *= 0.76
+        self.y_spd *= 0.76
         self.animation_count += 1
-        if self.animation_count > 3:
+        if self.animation_count > 4:
             self.animation_count = 0
             self.sprite_frame = (self.sprite_frame + 1) % self.num_sprites
 
     def calculate_tile(self):
         return (self.x + self.x_offset) // constants.game['tile_size'] + constants.game['num_tiles'][0] \
                * ((self.y + self.y_offset) // constants.game['tile_size'])
+
+    def stop(self):
+        self.x_spd = 0
+        self.y_spd = 0
+        self.key_right = False
+        self.key_left = False
+        self.key_down = False
+        self.key_up = False
 
 
 class Factory:
@@ -55,22 +76,38 @@ class Factory:
         self.y = constants.factories[name]['y_coord']
         self.grid_pos = constants.factories[name]['grid_pos']
         self.building_num = constants.factories[name]['bldg_num']
-        self.num_product = 0
+        self.operation1 = True
+        self.num_product1 = 0
+        self.num_product2 = 0
         self.fuel1 = 0
         self.fuel2 = 0
         self.fuel1_type = constants.factories[name]['fuel1_type']
         self.fuel2_type = constants.factories[name]['fuel2_type']
-        self.level = 0
+        self.fuel1_ipo = constants.factories[name]['fuel1_ipo']
+        self.fuel2_ipo = constants.factories[name]['fuel2_ipo']
+        self.level = 1
+        self.numY_offset = constants.factories[name]['numY_offset']
+        self.num1_offset = constants.factories[name]['num1_offset']
+        self.num2_offset = constants.factories[name]['num2_offset']
+        self.num3_offset = constants.factories[name]['num3_offset']
         self.on = False
         self.animation_rate = constants.factories[name]['animation_rate']
         self.sprite_frame = 0
         self.num_sprites = len(self.on_sprites)
 
     def produce(self):
-        self.num_product += 1
+        if self.operation1 and self.fuel1 >= self.fuel1_ipo:
+            self.fuel1 -= self.fuel1_ipo
+            self.num_product1 += 1
+        elif self.fuel2 >= self.fuel1_ipo:
+            self.fuel2 -= self.fuel2_ipo
+            self.num_product2 += 1
 
     def upgrade(self, amount):
         self.level += amount
+
+    def switch_operation(self):
+        self.operation1 = not self.operation1
 
     def flip_switch(self):
         self.on = not self.on
@@ -84,10 +121,9 @@ class Factory:
 class ResourceManager:
     def __init__(self):
         self.resources = []
-        with open('savestate.txt', 'r') as f:
-            x = json.load(f)
-        for i in range(len(constants.resources)):
-            self.resources.append(x[str(i)])
+        self.can_build = []
+        for fact in constants.factories.values():
+            self.can_build.append(fact['grid_pos'])
 
     def increase(self, resource, amount):
         self.resources[resource] += amount
